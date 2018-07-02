876ab46ee1457b78acceea9318b26709e05b04e1
Marcin Czekala <marcin_czekala@icloud.com>
2018-07-02 22:20:01 +0200

    add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var url = 'httdp://api.icndb.com/jokes/random';
+var url = 'httswp://api.icndb.com/jokes/random';
 getJoke();
 var button = document.getElementById('get-joke');
 button.addEventListener('click', function(){
@@ -8,14 +8,11 @@ button.addEventListener('click', function(){
 var paragraph = document.getElementById('joke');
 
 function getJoke() {
-	// if (request.status == 200) {
-	// 	paragraph.innerHTML = "200";
-	// }czemu nie moge ifa
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
-  xhr.onerror = function(event){
+  xhr.addEventListener('error', function(){
   	paragraph.innerHTML = "error";
-  };
+  });
   xhr.addEventListener('load', function(){
     var response = JSON.parse(xhr.response);
     paragraph.innerHTML = response.value.joke;
